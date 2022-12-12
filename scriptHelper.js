bbf4608a695c38cb56312e74f4ed1c8c52d8d620
Anjana_Asokan <anjanaasokan007@gmail.com>
2022-12-12 12:56:06 +0530

    defined formsubmission function and validateInput

--- scriptHelper.js
+++ scriptHelper.js
@@ -17,11 +17,44 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+    if(testInput==""){
+        return "Empty"
+    }
+    else if(isNaN(testInput)) {
+        return "Not a Number"
+    }
+   else{
+    return "Is a Number"
+   }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+
+    let pilotStatus = document.getElementById("pilotStatus");
+    pilotStatus.innerHTML = `Pilot ${pilot} Ready`;
+    let copilotStatus = document.getElementById("copilotStatus");
+    copilotStatus.innerHTML = `Co-pilot ${copilot} Ready`;
+
+    let launchStatus = document.getElementById("launchStatus");
+    launchStatus.innerHTML = "Shuttle ready for launch";
+    launchStatus.style.color = "green";
+
+    if(fuelLevel<10000){
+      let fuelStatus = document.getElementById("fuelStatus") ;
+      fuelStatus.innerHTML = "Not enough fuel for the journey";
+      launchStatus.innerHTML = "Shuttle not ready for launch";
+      launchStatus.style.color = "red";
+    } 
+
+    if(cargoLevel>10000){
+      let cargoStatus = document.getElementById("cargoStatus");
+      cargoStatus.innerHTML = "Too much mass for the shuttle to take off";
+      launchStatus.innerHTML = "Shuttle not ready for launch";
+      launchStatus.style.color = "red";
+   }
+   document.getElementById("launchStatusCheck").style.visibility = "";
+
+    
 }
 
 async function myFetch() {
